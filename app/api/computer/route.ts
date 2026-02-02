b8e6e410116768342ebd9d0af1b0a4b87d9daf7a
Ashitesh <raghuvesh1285@gmail.com>
2026-02-02 16:08:12 +0530

    Update route.ts

--- app/api/computer/route.ts
+++ app/api/computer/route.ts
@@ -1,4 +1,4 @@
-import { GoogleGenAI, ThinkingLevel } from "@google/genai";
+import { GoogleGenAI, ThinkingLevel } from "@google/genai";;
 import { NextResponse } from "next/server";
 import { writeFile } from "fs/promises";
 import path from "path";
